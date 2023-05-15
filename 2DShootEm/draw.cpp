c532a08f8956dc7e673de9fd0521b10e09684413
Edib Supic <edibce@gmail.com>
2023-05-15 16:54:43 +0200

    #transfer

---
+++ 2DShootEm/draw.cpp
@@ -0,0 +1,8 @@
+#include "../Dependencies/SDL2/include/SDL.h"
+
+#include "draw.h"
+
+void prepareScene(void)
+{
+	
+}
---
+++ 2DShootEm/input.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "../Dependencies/SDL2/include/SDL.h"
+
+#include "input.h"
+
+void doInput(void) 
+{
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event)) 
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			std::cout << "SDL_QUIT case" << std::endl;
+			break;
+		default:
+			break;
+		}
+	}
+}
